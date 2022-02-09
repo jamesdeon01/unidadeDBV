ec3fdd99e0142ca58b160cdde9f0ec626b9b9aaa
James Deon <jamesdeon01@gmail.com>
2022-02-09 19:02:16 -0300

    opa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-
+import React, { Component } from 'react';
 import './App.css';
+import {Layout, Navigation, Header, Drawer, Content, Tab, HeaderRow,HeaderTabs} from 'react-mdl';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Pagina Inicial
-      </h1>
-    </div>
-  );
+class App extends Component() {
+  render()
+  {
+    return (
+      <div style={{height: '300px', position: 'relative'}}>
+      <Layout fixedHeader fixedTabs>
+         <Header>
+            <HeaderRow title="Title" />
+            <HeaderTabs ripple activeTab={1} onChange={(tabId) => {}}>
+                <Tab>Tab1</Tab>
+                <Tab>Tab2</Tab>
+                <Tab>Tab3</Tab>
+            </HeaderTabs>
+          </Header>
+          <Drawer title="Title" />
+          <Content>
+            <div className="page-content">You can add logic to update the content of this container based on the "activeTab" receive in the `onChange` callback.</div>
+        </Content>
+    </Layout>
+</div>
+  
+    );
+  }
 }
 
 export default App;
